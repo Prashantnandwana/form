e38f3ba03ddacaf1d2cf0ddede15262ea025e7bd
F7 <7442774@gmail.com>
2023-10-26 19:28:10 +0530

    deco cont

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -17,9 +17,15 @@ class _MyHomePageState extends State<MyHomePage> {
         key: _formkey, child: Column(
         children: [
           Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image(image: assets/images/passimg.JPG),
+          ),
+          // AssetImage("assets/images/passimg.JPG"),
+          Padding(
             padding: const EdgeInsets.all(20.0),
             child: TextFormField(
               decoration: InputDecoration(
+                prefixIcon: Icon(Icons.email),
                 hintText: "Email",
                 border: OutlineInputBorder(),
               ),
@@ -29,18 +35,24 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
           ),
-          TextFormField(
-        decoration: InputDecoration(
-          labelText:"password", 
-        ),
-            obscureText: true,
-            onChanged: (value){password=value;},
-            validator:(value){
-              if(value!.isEmpty){return "Password cannot be empty";}
-              if(value.length<6){
-                return "password should have atleast 6 digits";
-              }
-            },
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextFormField(
+                  decoration: InputDecoration(
+                    
+                    prefixIcon: Icon(Icons.lock),
+            labelText:"password", 
+            hintText: "inter your password"
+                  ),
+              obscureText: true,
+              onChanged: (value){password=value;},
+              validator:(value){
+                if(value!.isEmpty){return "Password cannot be empty";}
+                if(value.length<6){
+                  return "password should have atleast 6 digits";
+                }
+              },
+            ),
           ),
           ElevatedButton(onPressed: (){if(
             _formkey.currentState!.validate()){
